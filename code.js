420a3981b62ad024b3487bfaa0d54fb5a257e392
savi-1311 <shambhavishandilya01@gmail.com>
2021-10-28 13:45:16 +0530

    fixed date error

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ var ist =  nd.toLocaleString();
 
 //Date in YYYY-MM-DD format
 var current_date = "";
-current_date = ist.slice(6,10)+"-"+ist.slice(0,2)+"-"+ist.slice(3,5);
+current_date = ist.slice(6,10)+"-"+ist.slice(3,5)+"-"+ist.slice(0,2);
 
 //Saving Logs to desktopList file
 console.log("🌸 Completed "+ GoalsData.days_items[0].total_completed+ " Tasks Today \n");
